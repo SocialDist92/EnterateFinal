8ad71fda9f389ed9d5e39910df5f5c91ffb072f2
Armando Rios Romano <armando@Armandos-MacBook-Pro.local>
2016-06-01 19:14:52 -0600

    multidex implementation

--- app/src/main/java/com/app/jonatan/enteratechihuahua/test/MyApplication.java
+++ app/src/main/java/com/app/jonatan/enteratechihuahua/test/MyApplication.java
@@ -3,6 +3,7 @@ package com.app.jonatan.enteratechihuahua.test;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by Jonatan on 06/05/2016.
@@ -10,6 +11,12 @@ import android.content.SharedPreferences;
 public class MyApplication extends Application {
 
     private static MyApplication sInstance;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
